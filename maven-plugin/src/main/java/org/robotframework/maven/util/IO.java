9d48901dcef9019e4bc707c26f5cf961966e2828
janne.t.harkonen <janne.t.harkonen@42ec9eaa-9077-11dd-b454-953cee25b285>
2008-10-31 13:24:53 +0000

    cleanup

--- maven-plugin/src/main/java/org/robotframework/maven/util/IO.java
+++ maven-plugin/src/main/java/org/robotframework/maven/util/IO.java
@@ -17,7 +17,6 @@ package org.robotframework.maven.util;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
