e004578fa6580a16aae535485f26b43ff07cd5d2
hhhulkko <hhhulkko@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-02-23 13:35:57 +0000

    Reverted all.

--- javalib-core/tags/javalib-core-0.8/trunk/src/test/java/org/robotframework/javalib/keyword/ConflictingKeyword.java
+++
@@ -1,9 +0,0 @@
-package org.robotframework.javalib.keyword;
-
-import org.robotframework.javalib.keyword.Keyword;
-
-public class ConflictingKeyword implements Keyword {
-    public Object execute(Object[] arguments) {
-        return "Classpath Keyword";
-    }
-}
--- javalib-core/tags/javalib-core-0.8/trunk/src/test/java/org/robotframework/javalib/keyword/SpringKeyword.java
+++
@@ -1,9 +0,0 @@
-package org.robotframework.javalib.keyword;
-
-import org.robotframework.javalib.keyword.Keyword;
-
-public class SpringKeyword implements Keyword {
-    public Object execute(Object[] arguments) {
-        return "Spring Keyword";
-    }
-}
