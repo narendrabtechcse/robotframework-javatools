c558e0d5d91b749275f834246705f9406f731082
hhhulkko <hhhulkko@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-02-27 13:23:04 +0000

    Added failing test for https support.

--- jvmconnector/trunk/src/test/java/org/robotframework/jvmconnector/util/FileServer.java
+++ jvmconnector/trunk/src/test/java/org/robotframework/jvmconnector/util/FileServer.java
@@ -2,21 +2,27 @@ package org.robotframework.jvmconnector.util;
 
 import java.io.File;
 
+import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.DefaultHandler;
 import org.mortbay.jetty.handler.HandlerList;
 import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.security.SslSocketConnector;
 
 public class FileServer {
-    public static final int PORT = 14563;
-    public static final String URL_BASE = "http://localhost:" + PORT;
+    public static final int HTTP_PORT = 14563;
+    public static final int HTTPS_PORT = 14564;
+    public static final String URL_BASE = "http://localhost:" + HTTP_PORT;
     private static Server server;
+    private static String resourceBase;
 
     public static void start(String resourceBase) throws Exception {
-        if (server != null && server.isStarted())
-            return;
-        server = new Server(PORT);
+        FileServer.resourceBase = resourceBase;
+        if (isStarted()) return;
+        server = new Server(HTTP_PORT);
+        server.setConnectors(new Connector[] {getPlainConnector(HTTP_PORT), getSslConnector(HTTPS_PORT)});
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setResourceBase(resourceBase);
         HandlerList handlers = new HandlerList();
@@ -38,4 +44,27 @@ public class FileServer {
         System.out.println(resourceBase);
         start(resourceBase);
     }
+    
+    private static SelectChannelConnector getPlainConnector(int port) {
+        SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setPort(port);
+        connector.setMaxIdleTime(30000);
+        return connector;
+    }
+    
+    private static SslSocketConnector getSslConnector(int port) {
+        SslSocketConnector sslConnector = new SslSocketConnector();
+        sslConnector.setKeystore(resourceBase + "/keystore");
+        sslConnector.setKeyPassword("pulkkisenjorma");
+        sslConnector.setPort(port);
+        sslConnector.setMaxIdleTime(30000);
+        return sslConnector;
+    }
+    
+    private static boolean isStarted() {
+        return server != null && server.isStarted();
+    }
 }
+
+
+
