8becc16e1c3a8e24bde869a9767058f3bb323f51
hhhulkko <hhhulkko@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-03-16 12:55:11 +0000

    Improved run_tests.py

--- jvmconnector/trunk/run_tests.py
+++ jvmconnector/trunk/run_tests.py
@@ -7,7 +7,7 @@ import re
 
 sys.path.insert(0, '/usr/lib/python2.5/site-packages')
 base = os.path.abspath(os.path.normpath(os.path.split(sys.argv[0])[0]))
-for path in [ "./src/main/python" ]:
+for path in [ "./src/main/python", "./src/test/python" ]:
     path = os.path.join(base, path.replace('/', os.sep))
     if path not in sys.path:
         sys.path.insert(0, path)
@@ -28,13 +28,43 @@ def get_tests(directory):
             modules.append(__import__(modname))
     tests.extend([ unittest.defaultTestLoader.loadTestsFromModule(module)
                    for module in modules ])
-    return tests        
+    return tests
+
+def exists(file_name):
+    file = os.path.join(base, file_name)
+    return os.path.exists(file)
+
+def sh(command):
+    process = os.popen(command)
+    output = process.read()
+    process.close()
+    return output
+
+def add_dependencies_to_path():
+    if not exists('dependencies.txt'):
+        mvn_output = sh("mvn dependency:list -DoutputAbsoluteArtifactFilename=true").splitlines()
+        jars = [re.sub('.*:', '', file) for file in mvn_output if re.search('jar', file)]
+        dependencies_txt = open(os.path.join(base, 'dependencies.txt'), 'w')
+        for jar in jars:
+            dependencies_txt.write(jar + "\n")
+
+    classes = os.path.join('target', 'classes')
+    test_classes = os.path.join('target', 'test-classes')
+    if not exists(classes) or not exists(test_classes):
+        sh("mvn test-compile")
+
+    dependencies = [classes, test_classes] + open('dependencies.txt', 'rb').read().splitlines()
+    os.environ['CLASSPATH'] = os.pathsep.join(dependencies)
 
 if __name__ == '__main__':
-    tests = get_tests(".")
-    suite = unittest.TestSuite(tests)
-    runner = unittest.TextTestRunner(descriptions=0, verbosity=1)
-    result = runner.run(suite)
-    rc = len(result.failures) + len(result.errors)
-    if rc > 250: rc = 250
-    sys.exit(rc)
+    if os.name == 'java':
+        tests = get_tests(".")
+        suite = unittest.TestSuite(tests)
+        runner = unittest.TextTestRunner(descriptions=0, verbosity=1)
+        result = runner.run(suite)
+        rc = len(result.failures) + len(result.errors)
+        if rc > 250: rc = 250
+        sys.exit(rc)
+    else:
+        add_dependencies_to_path()
+        os.system("jython %s" % __file__)
