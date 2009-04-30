aa03357816ff21ec97d3ab0b2b1e358878fe746e
hhhulkko <hhhulkko@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-04-30 09:52:37 +0000

    [maven-release-plugin]  copy for tag jvmconnector-0.7.4

---
+++ jvmconnector/tags/jvmconnector-0.7.4/src/test/java/RMILauncherSpec.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2008 Nokia Siemens Networks Oyj
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import jdave.Specification;
+import jdave.junit4.JDaveRunner;
+
+import org.jmock.Expectations;
+import org.junit.runner.RunWith;
+import org.laughingpanda.beaninject.Inject;
+import org.robotframework.jvmconnector.server.ApplicationLauncher;
+import org.robotframework.jvmconnector.server.RmiService;
+
+@RunWith(JDaveRunner.class)
+public class RMILauncherSpec extends Specification<RMILauncher> {
+    private String javaExecutable = "java";
+    private String jvmArgs = "-Dfoo=bar";
+    private String applicationClassName = "com.acme.SomeApp";
+    private String rmiConfigFilePath = "rmiConfig.xml";
+    
+    public class ActingAsLibrary {
+        private Runtime runtime;
+        private Runtime originalRuntime;
+
+        public RMILauncher create() {
+            originalRuntime = Runtime.getRuntime();
+            runtime = mock(Runtime.class);
+            injectRuntime(runtime);
+            return new RMILauncher(javaExecutable, jvmArgs);
+        }
+        
+        public void executesItselfWithGivenArgs() throws Exception {
+            String applicationArgs = "foo bar baz";
+            
+            final String expectedCommand = javaExecutable + " " + jvmArgs + " " + RMILauncher.class.getName()
+                + " " + rmiConfigFilePath + " " + applicationClassName + " " + applicationArgs;
+            
+            checking(new Expectations() {{
+                one(runtime).exec(expectedCommand);
+            }});
+            
+            context.startApplicationAndRMIService(rmiConfigFilePath, applicationClassName, new String[] {"foo", "bar", "baz" });
+        }
+        
+        public void destroy() {
+            injectRuntime(originalRuntime);
+        }
+        
+        private void injectRuntime(Runtime runtime) {
+            Inject.staticField("currentRuntime").of(Runtime.class).with(runtime);
+        }
+    }
+    
+    public class WhenExecuted {
+        private RmiService rmiService;
+        private ApplicationLauncher applicationLauncher;
+
+        public void create() {
+            rmiService = injectMock(RmiService.class, "rmiService", RMILauncher.class);
+            applicationLauncher = injectMock(ApplicationLauncher.class, "applicationLauncher", RMILauncher.class);
+        }
+        
+        public void startsRmiService() throws Exception {
+            checking(new Expectations() {{
+                one(rmiService).start(rmiConfigFilePath);
+                ignoring(applicationLauncher);
+            }});
+            
+            RMILauncher.main(new String[] {rmiConfigFilePath, applicationClassName });
+        }
+        
+        public void startsApplication() throws Exception {
+            checking(new Expectations() {{
+                one(applicationLauncher).launchApplication(applicationClassName, new String[] {"foo", "bar"});
+                ignoring(rmiService);
+            }});
+            
+            RMILauncher.main(new String[] {rmiConfigFilePath, applicationClassName, "foo", "bar"});
+        }
+        
+        private <T> T injectMock(Class<T> mockedClass, String fieldName, Class<?> target) {
+            T mockedDependency = mock(mockedClass);
+            Inject.staticField(fieldName).of(target).with(mockedDependency);
+            return mockedDependency;
+        }
+    }
+}
---
+++ jvmconnector/tags/jvmconnector-0.7.4/src/test/java/org/robotframework/jvmconnector/keyword/JvmconnectorKeywords.java
@@ -0,0 +1,46 @@
+package org.robotframework.jvmconnector.keyword;
+
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywords;
+import org.robotframework.javalib.library.AnnotationLibrary;
+import org.robotframework.jvmconnector.launch.jnlp.JnlpEnhancer;
+import org.robotframework.jvmconnector.mocks.MyApplication;
+
+
+@RobotKeywords
+public class JvmconnectorKeywords extends AnnotationLibrary {
+    public JvmconnectorKeywords() {
+        super("org/robotframework/jvmconnector/keyword/**/*.class");
+    }
+    
+    @RobotKeyword
+    public boolean applicationIsRunning() {
+        return MyApplication.isRunning;
+    }
+    
+    @RobotKeyword
+    public void stopApplication() {
+        MyApplication.isRunning = false;
+    }
+    
+    @RobotKeyword
+    public String[] getArguments() {
+        return MyApplication.args;
+    }
+    
+    @RobotKeyword
+    public void stopJvm() {
+        System.exit(0);
+    }
+    
+    @RobotKeyword
+    public String getEnhancedJnlp(String libraryResourceDir, String rmiConfigFilePath, String jnlpUrl) throws Exception {
+        JnlpEnhancer jnlpRunner = new JnlpEnhancer(libraryResourceDir);
+        String pathToJnlp = jnlpRunner.createRmiEnhancedJnlp(rmiConfigFilePath, jnlpUrl);
+        return FileUtils.readFileToString(new File(pathToJnlp), "UTF-8");
+    }
+}
