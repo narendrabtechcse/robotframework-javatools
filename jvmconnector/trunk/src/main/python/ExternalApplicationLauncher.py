8b3972a043cbf7f653f2809846f9fe7632486fb9
hhhulkko <hhhulkko@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-05-06 13:09:33 +0000

    java to ApplicationLauncher started.

--- jvmconnector/trunk/src/main/python/ExternalApplicationLauncher.py
+++ jvmconnector/trunk/src/main/python/ExternalApplicationLauncher.py
@@ -4,12 +4,8 @@ import os
 from os.path import abspath, dirname, exists, join, normpath
 import sys
 
-#JAVA = '/home/rantanen/opt/jdk1.5.0_16//bin/java'
 
-class RMILauncher:
-
-    def __init__(self):
-        self.port_info_file = join(dirname(__file__), 'port.txt')
+class ExternalApplicationLauncher:
 
     def launch_java_with_rmi(self, args):
         print args
@@ -18,11 +14,24 @@ class RMILauncher:
         os.popen2(command)
 
     def _get_command(self, args):
-        if 'RMILauncher' in args:
+        if 'ApplicationLauncher.py' in args:
             return '%s %s' % (self._get_java(), ' '.join(args))
         else:
-            return  ' '.join( [self._get_java(), 'RMILauncher', 
-                               self._get_xml() ] + args) 
+            return  ' '.join( [self._get_java(), 'ApplicationLauncher.py'] + args) 
+
+    def _get_jython(self, args):
+        script = join(dirname(__file__), 'ApplicationLauncher.py')
+        jvm_args, application, app_args = self._resolve_arguments(args)
+        command = 'jython -Dpython.path=%s %s %s %s %s' % (self._get_python_path(),
+                  jvm_args, script, self.application, args)
+
+    def _resolve_arguments(self, args):
+        
+        
+    def _get_python_path(self):
+        for path_entry in sys.path:
+            if exists(join(path_entry, 'robot')):
+                return path_entry
 
     def _get_java(self):
         fake_java_dir = normpath(abspath(dirname(__file__)))
@@ -33,48 +42,6 @@ class RMILauncher:
                     print java_path
                     return java_path
     
-    def _get_xml(self):
-        port = self._get_port()
-        xml_path = self._generate_xml(port)
-        self._write(self.port_info_file, str(port))
-        return xml_path
-        
-    def _get_port(self):
-        if not exists(self.port_info_file):
-            return 11100
-        f = open(self.port_info_file, 'r')
-        content = f.read().strip()
-        f.close()
-        return int(content) + 1
-        
-    def _write(self, path, content):
-        f = open(path, 'w')
-        f.write(content)
-        f.close()
-
-    def _generate_xml(self, port):
-        content = """<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE beans PUBLIC "-//SPRING//DTD BEAN//EN"
-        "http://www.springframework.org/dtd/spring-beans.dtd">
-<beans>
-  <bean id="library" class="SwingLibrary" />
-
-  <bean id="rmiService" class="org.robotframework.jvmconnector.server.SimpleRobotRmiService">
-    <property name="library" ref="library" />
-  </bean>
-
-  <bean id="serviceExporter" class="org.springframework.remoting.rmi.RmiServiceExporter">
-    <property name="serviceName" value="SwingLibrary" />
-    <property name="service" ref="rmiService" />
-    <property name="registryPort" value="%s" />
-    <property name="serviceInterface" value="org.robotframework.jvmconnector.server.RobotRmiService" />
-  </bean>
-</beans>
-""" % (port)
-        xml_path = join(dirname(__file__), '..', 'configs', 'config_%s.xml' % (port))
-        self._write(xml_path, content)
-        return xml_path
-        
 if __name__ == '__main__':
-    launcher = RMILauncher()
+    launcher = ExternalApplicationLauncher()
     launcher.launch_java_with_rmi(sys.argv[1:])
