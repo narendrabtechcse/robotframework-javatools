3e9ab27c6a1c2a68da836f1ee74fd1c778061f3b
jprantan <jprantan@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-08-24 10:50:43 +0000

    Initial version of the javaagent implementation.

--- jvmconnector/trunk/src/main/java/org/robotframework/jvmconnector/server/RmiServiceAgent.java
+++ jvmconnector/trunk/src/main/java/org/robotframework/jvmconnector/server/RmiServiceAgent.java
@@ -1,15 +1,29 @@
 package org.robotframework.jvmconnector.server;
 
+import java.io.IOException;
 import java.lang.instrument.Instrumentation;
+import java.util.jar.JarFile;
+
 
 public class RmiServiceAgent {
-
-	public static void premain(String agentArguments) {
-		new RmiService().start("/tmp/launcher.txt");
-	}
-	
+		
 	public static void premain(String agentArguments, Instrumentation inst) {
-		premain(agentArguments);
-	}
-	
+		System.out.println("Starting Agent");
+		if (agentArguments != null) {
+			String[] paths = agentArguments.split(":");
+			for (String path : paths) {
+				try {
+					System.out.println("Appending path: " + path);
+
+					inst.appendToSystemClassLoaderSearch(new JarFile(path));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		new RmiService().start("/tmp/launcher.txt");
+
+		System.out.println("Agent started");
+	}	
 }
