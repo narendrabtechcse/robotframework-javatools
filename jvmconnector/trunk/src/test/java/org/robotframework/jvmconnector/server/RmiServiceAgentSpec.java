50306a1f6773bd78c20101bf43b4b358240e7b39
hhhulkko <hhhulkko@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-08-26 07:34:27 +0000

    Added spec for RmiServiceAgent.

---
+++ jvmconnector/trunk/src/test/java/org/robotframework/jvmconnector/server/RmiServiceAgentSpec.java
@@ -0,0 +1,19 @@
+package org.robotframework.jvmconnector.server;
+
+import jdave.Specification;
+import jdave.junit4.JDaveRunner;
+
+import org.junit.runner.RunWith;
+
+@RunWith(JDaveRunner.class)
+public class RmiServiceAgentSpec extends Specification<RmiServiceAgent> {
+    public class Any {
+        public RmiServiceAgent create() {
+            return new RmiServiceAgent();
+        }
+        
+        public void foo() {
+            
+        }
+    }
+}
