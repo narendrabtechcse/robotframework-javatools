cc8bebbf6048913ad27f5006a8e44c28758cce7e
hhhulkko <hhhulkko@42ec9eaa-9077-11dd-b454-953cee25b285>
2009-09-06 09:46:22 +0000

    Works on Windows.

--- examples/javaws-demo/demo/run.py
+++ examples/javaws-demo/demo/run.py
@@ -9,8 +9,10 @@ def main(args):
     testlibdir = os.path.join(dir, 'test-lib')
     jars = glob(os.path.join(testlibdir, '*.jar'))
     os.environ['CLASSPATH'] = os.pathsep.join(jars)
+    if os.name == 'nt' and not os.getenv('HOME'):
+        os.environ['HOME'] = os.environ['USERPROFILE'] 
     outputdir = os.path.join(dir, 'results')
-    os.system("jybot --pythonpath %s --loglevel TRACE --outputdir \"%s\" %s" %
+    os.system('jybot --pythonpath "%s" --loglevel TRACE --outputdir "%s" %s' %
     (testlibdir, outputdir, ' '.join(args)))
 
 if __name__ == '__main__':
