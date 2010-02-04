4ed80243c95ff9c9cc7b63aacbc101146245d783
jprantan <jprantan@42ec9eaa-9077-11dd-b454-953cee25b285>
2010-02-04 05:44:19 +0000

    Updated to use uberjar when running unit and acceptance tests.

--- jvmconnector/trunk/run_tests.py
+++ jvmconnector/trunk/run_tests.py
@@ -8,8 +8,8 @@ import re
 from tempfile import gettempdir
 
 base = os.path.abspath(os.path.normpath(os.path.split(sys.argv[0])[0]))
-python_main = os.path.join('src', 'main', 'python')
-python_test = os.path.join('src', 'test', 'python')
+python_main = os.path.join(base, 'src', 'main', 'python')
+python_test = os.path.join(base, 'src', 'test', 'python')
 testfile = re.compile('^test_.*\.py$', re.IGNORECASE)          
 
 def get_tests(directory):
@@ -38,16 +38,17 @@ def sh(command):
     process.close()
     return output
 
-def add_dependencies_to_path():
+def add_src_and_test_to_pythonpath():
     for path in [ python_main, python_test ]:
-        path = os.path.join(base, path)
         if path not in sys.path:
             sys.path.insert(0, path)
+    os.environ['PYTHONPATH'] = os.pathsep.join(sys.path)
 
+def add_dependencies_to_classpath():
     if not exists('dependencies.txt'):
         os.environ['MAVEN_OPTS'] = '-DoutputAbsoluteArtifactFilename=true'
         mvn_output = sh('mvn dependency:list').splitlines()
-        
+
         jars = [re.sub('.*:((:?C:)?)', '\\1', file) for file in mvn_output if re.search('jar', file)]
         dependencies_txt = open(os.path.join(base, 'dependencies.txt'), 'w')
         for jar in jars:
@@ -55,15 +56,16 @@ def add_dependencies_to_path():
                 dependencies_txt.write(jar + '\n')
         dependencies_txt.flush()
 
-    classes = os.path.join('target', 'classes')
     test_classes = os.path.join('target', 'test-classes')
-    if not exists(classes) or not exists(test_classes):
+    if not exists(test_classes):
         sh('mvn test-compile')
 
-    dependencies = [classes, test_classes] + open('dependencies.txt', 'rb').read().splitlines()
+    dependencies =  [get_jvmconnector_jar()] + [test_classes] + get_test_deps()
+    os.environ['CLASSPATH'] = os.pathsep.join(dependencies)
 
-    os.environ['CLASSPATH'] = os.pathsep.join(dependencies + [get_jvmconnector_jar()])
-    os.environ['PYTHONPATH'] = os.pathsep.join(sys.path)
+def get_test_deps():
+    deps = open('dependencies.txt', 'rb').read().splitlines()
+    return [ dep for dep in deps if 'org/robotframework/' in dep or 'org/mortbay' in dep]
 
 def get_jvmconnector_jar():
     pattern = os.path.join(os.path.dirname(__file__),
@@ -75,30 +77,39 @@ def get_jvmconnector_jar():
     else:
         raise RuntimeError('Please run "mvn assembly:assembly first')
 
-def get_python_path():
+def get_robot_installation_path():
     for path in sys.path:
         if os.path.exists(os.path.join(path, 'robot')):
             return path
-    
-if __name__ == '__main__':
-    rc = 0
-    if os.name == 'java':
-        sys.path.append(python_main)
-        tests = get_tests(os.path.join(base, 'src','test', 'python'))
-        suite = unittest.TestSuite(tests)
-        runner = unittest.TextTestRunner(descriptions=0, verbosity=1)
-        result = runner.run(suite)
-        rc = len(result.failures) + len(result.errors)
-        if rc > 250: rc = 250
-    else:
-        add_dependencies_to_path()
-        python_path = get_python_path()
-        if len(sys.argv[1:]) > 0:
-            runner = os.path.join(python_path, 'robot', 'runner.py')
-            args_as_string = ' '.join(sys.argv[1:])
-            command = 'jython -Dpython.path="%s" "%s" --debugfile debug.txt --loglevel TRACE --noncritical javaagent --outputdir %s %s' % (python_path, runner, gettempdir(), args_as_string)
-            rc = os.system(command)
-        else:
-            rc = os.system('jython -Dpython.path=%s %s' % (python_path, __file__))
 
+def run_unit_tests():
+    sys.path.append(python_main)
+    tests = get_tests(os.path.join(base, 'src','test', 'python'))
+    suite = unittest.TestSuite(tests)
+    runner = unittest.TextTestRunner(descriptions=0, verbosity=1)
+    result = runner.run(suite)
+    rc = len(result.failures) + len(result.errors)
+    if rc > 250: rc = 250
+    return rc
+
+def run_robot_tests(args):
+    runner = os.path.join(get_robot_installation_path(), 'robot', 'runner.py')
+    args_as_string = ' '.join(args)
+    command = 'jython -Dpython.path="%s" "%s" --debugfile debug.txt --loglevel TRACE --noncritical javaagent --outputdir %s %s' % (get_robot_installation_path(), runner, gettempdir(), args_as_string)
+    return os.system(command)
+
+def run_unit_tests_with_jython():
+    return os.system('jython -Dpython.path=%s %s' % (get_robot_installation_path(), __file__))
+
+
+if __name__ == '__main__':
+    if os.name == 'java':
+        rc = run_unit_tests()
+    else:
+        add_src_and_test_to_pythonpath()
+        add_dependencies_to_classpath()
+        if len(sys.argv[1:]) > 0:
+            rc = run_robot_tests(sys.argv[1:])
+        else:
+            rc = run_unit_tests_with_jython()
     sys.exit(rc)
