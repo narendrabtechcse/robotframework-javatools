bc895797d345e27ead419cd449b48b446c27fdb1
jprantan <jprantan@42ec9eaa-9077-11dd-b454-953cee25b285>
2010-02-15 13:20:25 +0000

    Fixed to work with changed made to the run_tests.py

--- jvmconnector/trunk/create_doc.py
+++ jvmconnector/trunk/create_doc.py
@@ -3,14 +3,13 @@
 import os
 import sys
 
-from run_tests import add_dependencies_to_path, get_python_path
+from run_tests import add_dependencies_to_classpath, get_robot_installation_path
 
 if __name__ == '__main__':
-    add_dependencies_to_path()
-    python_path = get_python_path()
+    add_dependencies_to_classpath()
     base = os.path.dirname(__file__)
     libdoc = os.path.join(base, 'doc', 'libdoc.py')
     output = os.path.join(base, 'doc', 'RemoteApplications.html')
     lib = os.path.join(base, 'src', 'main', 'python', 'RemoteApplications.py')
-    command = 'jython -Dpython.path=%s %s --output %s %s' % (python_path, libdoc, output, lib)
+    command = 'jython -Dpython.path=%s %s --output %s %s' % (get_robot_installation_path(), libdoc, output, lib)
     sys.exit(os.system(command))
