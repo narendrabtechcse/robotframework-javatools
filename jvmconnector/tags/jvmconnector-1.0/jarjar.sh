49ac532461717b12b441373ffec6de9696fa6bf0
KariHusa <KariHusa@42ec9eaa-9077-11dd-b454-953cee25b285>
2010-02-16 11:20:49 +0000

    1.0 Release

---
+++ jvmconnector/tags/jvmconnector-1.0/jarjar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+target=`pwd`/target
+
+set -o verbose
+rm -rf target
+mvn assembly:assembly
+jar=`echo $target/jvmconnector-*-SNAPSHOT-jar-with-dependencies.jar`
+jarjar=${jar%.*}-jarjar.jar
+java -jar ${target}/../release/jarjar-1.0.jar process ${target}/../release/jarjar_rules.txt $jar $jarjar
+unzip -d $target $jar META-INF/MANIFEST.MF
+
+tmpdir=$target/tmp
+mkdir $tmpdir
+unzip -d $tmpdir $jarjar
+rmic -verbose -classpath $tmpdir -d $tmpdir org.robotframework.org.springframework.remoting.rmi.RmiInvocationWrapper
+rmic -verbose -iiop -always -classpath $tmpdir -d $tmpdir org.robotframework.org.springframework.remoting.rmi.RmiInvocationWrapper
+rm $jarjar
+jar -cfm $jarjar $target/META-INF/MANIFEST.MF -C $tmpdir .
+mv $jarjar $jar
+rm -rf $tmpdir
+rm -rf $target/META-INF
